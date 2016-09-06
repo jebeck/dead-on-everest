899f11b13efcdd78eada094a594ffad83a6e92d5
Jana E. Beck <jana.eliz.beck@gmail.com>
2016-09-06 00:05:59 -0700

    text tweaks

--- src/App.js
+++ src/App.js
@@ -63,13 +63,13 @@ class App extends Component {
         text: 'British eccentric Maurice Wilson died on Everest in 1934 after sneaking into Tibet to make his attempt. He was an inexperienced climber who made no attempt to learn the necessary skills or even obtain required equipment, such as crampons for walking on glacier ice.',
       }, {
         id: 'year1953',
-        text: 'In 1953, Edmund Hillary and Tenzing Norgay became the first climbers to reach the summit of Mount Everest and return alive.',
+        text: 'In 1953, Edmund Hillary and Tenzing Norgay became the first climbers to reach the summit of Mount Everest. They descended safely to great celebration, including a knighthood for Hillary and the George Medal for Norgay.',
       }, {
         id: 'year1996',
         text: 'The disastrous 1996 climbing season on Mount Everest was recorded most famously in Jon Krakauer\'s book Into Thin Air.',
       }, {
         id: 'year2006',
-        text: '2006 saw both the controversial death of solo British climber David Sharp, passed by many other climbers ascending to and descending from the summit as he slowly died in the Green Boots cave, and the heroic rescue of Lincoln Hall, who against all odds survived a bivouac in the Death Zone and was discovered the next day by a four-person team led by Daniel Mazur of the U.S.; the team abandoned their own summit attempt and organized a rescue team of about a dozen Sherpas to help Hall descend.',
+        text: '2006 yielded both the controversial death of solo British climber David Sharp, passed by many other climbers ascending to and descending from the summit as he slowly died in the Green Boots cave, and the heroic rescue of Lincoln Hall, who against all odds survived a bivouac in the Death Zone and was discovered the next day by a four-person team led by Daniel Mazur of the U.S.; the team abandoned their own summit attempt and organized a rescue team of about a dozen Sherpas to help Hall descend.',
       }, {
         id: 'year2015',
         text: 'The earthquake in Nepal in April 2015 caused an avalanche at Everest Base Camp, making 2015 the deadliest year to date on the mountain.',
